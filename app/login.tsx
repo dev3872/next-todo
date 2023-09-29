3bd5c0f43d9c40ab2266237ce87c8ae709b0046b
Rohit Bhatt <rohitbhatt@Rohits-MacBook-Air.local>
2023-09-29 14:10:19 +0530

    auth0 init

---
+++ app/login.tsx
@@ -0,0 +1,10 @@
+"use client";
+export default function Login() {
+  return (
+    <div className="flex w-full h-full justify-center content-center">
+      <div>
+        <a href="/api/auth/login">Login</a>
+      </div>
+    </div>
+  );
+}
